a758170a4cf4b98fe17b9201940d5548193fe737
Manish Chauhan <manish.chauhan@ushatech.in>
2024-05-08 20:31:25 +0530

    Add passwords using InMemoryUserDetailsManager closes #11

--- spring_security/src/main/java/in/ushatech/spring_security/config/SecurityConfig.java
+++ spring_security/src/main/java/in/ushatech/spring_security/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package in.ushatech.spring_security.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -12,7 +15,10 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class SecurityConfig
 {
-
+    /**
+     * We need to first give authorization for the  uris by creating a bean of SecurityFilterChain
+     * The we need to provide UserDetailsManager
+     * */
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests
@@ -23,4 +29,21 @@ public class SecurityConfig
                 .httpBasic(withDefaults());
         return http.build();
     }
+
+    @Bean
+    public InMemoryUserDetailsManager getInMemoryUserDetailsManager()
+    {
+        UserDetails userDetails= User.withUsername("manish")
+                .password("simple").authorities("user")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+    
+    @SuppressWarnings("deprecation")
+    @Bean
+    public PasswordEncoder getPasswordEncoder()
+    {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
