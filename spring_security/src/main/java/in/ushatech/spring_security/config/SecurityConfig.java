916c93c4ed79445ab2c624a9b31d0d4c95328c03
Manish Chauhan <manish.chauhan@ushatech.in>
2024-05-08 20:13:37 +0530

    Add lambda style SecurityFilterChain bean to create for the following configuration closes #13

--- spring_security/src/main/java/in/ushatech/spring_security/config/SecurityConfig.java
+++ spring_security/src/main/java/in/ushatech/spring_security/config/SecurityConfig.java
@@ -1,12 +1,26 @@
 package in.ushatech.spring_security.config;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig
 {
 
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests
+                        (
+                (authorize) -> authorize.requestMatchers("/myAccount","/myCards","/myBalance","/myLoans").authenticated()
+                        .requestMatchers("/notices","/contact").permitAll()
+                    ).formLogin(withDefaults())
+                .httpBasic(withDefaults());
+        return http.build();
+    }
 }
