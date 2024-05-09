a4356fc01c5f9a075ceee92ec00138a6b19e8ef4
Manish Chauhan <manish.chauhan@ushatech.in>
2024-05-09 22:05:14 +0530

    Add post api for registering a user closes #22

---
+++ spring_security/src/main/java/in/ushatech/spring_security/controller/LoginController.java
@@ -0,0 +1,39 @@
+package in.ushatech.spring_security.controller;
+
+import in.ushatech.spring_security.entity.Customer;
+import in.ushatech.spring_security.repository.CustomerRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController
+{
+    private final CustomerRepository customerRepository;
+
+    public LoginController(CustomerRepository customerRepository)
+    {
+        this.customerRepository = customerRepository;
+    }
+    @PostMapping
+    public ResponseEntity<String> register(@RequestBody Customer customer)
+    {
+        ResponseEntity<String> response=null;
+        Customer customer1 ;
+        try
+        {
+            customer1= customerRepository.save(customer);
+            if(customer1.getId()>0)
+            {
+                response= ResponseEntity.status(HttpStatus.CREATED).body("User created successfully");
+            }
+        }
+        catch (Exception e)
+        {
+            response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong at server");
+        }
+        return response;
+    }
+}
--- spring_security/src/main/java/in/ushatech/spring_security/repository/CustomerRepository.java
+++ spring_security/src/main/java/in/ushatech/spring_security/repository/CustomerRepository.java
@@ -2,11 +2,13 @@ package in.ushatech.spring_security.repository;
 
 import in.ushatech.spring_security.entity.Customer;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@Component
 public interface CustomerRepository extends CrudRepository<Customer, Integer>
 {
     List<Customer> findByEmail(String email);
