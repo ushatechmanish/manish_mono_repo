2ac5c5f3dc0ce2bbc113e1ed0b84f081d784325d
Manish Chauhan <manish.chauhan@ushatech.in>
2024-05-15 23:08:56 +0530

    Make a spring boot 2.7 submodule and learn the deprecated features also closes closes #54

---
+++ spring_boot2_7/src/main/java/in/ushatech/spring_boot2_7/SpringBoot27Application.java
@@ -0,0 +1,13 @@
+package in.ushatech.spring_boot2_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot27Application.class, args);
+	}
+
+}
---
+++ spring_boot2_7/src/test/java/in/ushatech/spring_boot2_7/SpringBoot27ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ushatech.spring_boot2_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
