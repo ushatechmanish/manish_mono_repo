635673320ef7bc528b82e34bb06e8f75a899c665
Manish Chauhan <manish.chauhan@ushatech.in>
2024-05-18 07:34:57 +0530

    Add Test profile closes #55

--- spring_boot2_7/src/main/java/in/ushatech/spring_boot2_7/SpringBoot27Application.java
+++ spring_boot2_7/src/main/java/in/ushatech/spring_boot2_7/SpringBoot27Application.java
@@ -2,12 +2,14 @@ package in.ushatech.spring_boot2_7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBoot27Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot27Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SpringBoot27Application.class, args);
+
 	}
 
 }
