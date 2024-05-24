0bf6c7511295004e001a84443f84a7924f949595
Manish Chauhan <manish.chauhan@ushatech.in>
2024-05-24 08:25:27 +0530

    shell : set variables closes #69 shell : try to print $* and $@ by escaping character closes #70

--- shell_scripting/practice.sh
+++ shell_scripting/practice.sh
@@ -6,11 +6,24 @@ echo "The current file name is $0"
 
 echo "The parameter 1 is $1"
 
+# shell : set variables
+
+echo "There should be no space before and after the = while setting variables "
+
+name=manish
+
+echo "Printing variable name "$name
 # Argument mixes string and array. Use * or separate argument.
 #echo "All arguments are $@"
 
 #preferred way $* treats all arguments as a single string
+echo "\"\$*\" treats all arguments as a single string"
+
 echo "All arguments are $*"
+
+echo  "\"\$@\" treats all arguments as different strings"
+echo "All arguments are $*"
+
 
 echo " Printing  each argument on a new line "
 # $@ treats all args as separate
@@ -18,3 +31,4 @@ for arg in "$@"
   do
     echo $arg
   done
+
