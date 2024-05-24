cd8adda68ee303b92e109ed7ecd7f3a8e6408f1a
Manish Chauhan <manish.chauhan@ushatech.in>
2024-05-24 07:29:03 +0530

    Given 2D array calculate the sum of diagonal elements. closes #58

--- java/dsa/src/main/java/udemy_course_questions/Exercise.java
+++ java/dsa/src/main/java/udemy_course_questions/Exercise.java
@@ -12,6 +12,15 @@ public class Exercise
         System.out.println(Arrays.toString(middle(new int[]{1,2,3,4,5})));
         System.out.println(Arrays.toString(middleUsingArrayMethod(new int[]{1,2,3,4,5})));
         System.out.println(Arrays.toString(removeDuplicates(new int[]{1,1,3,1,5})));
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int sum = calculateArrayDiagonalElementsSum(matrix);
+        System.out.println("Sum of diagonal elements: " + sum); // Output should be 25
+
     }
     public static int[] middle(int[] array)
     {
@@ -51,4 +60,22 @@ public class Exercise
         }
         return result;
     }
+
+    public static int calculateArrayDiagonalElementsSum(int[][] arr)
+    {
+        if(arr==null || arr.length==0) return 0;
+        if(arr.length==1) return arr[0][0];
+        int result=0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            result+=arr[i][i];
+            if(i!= arr.length-1-i)
+            {
+                result+=arr[i][arr.length-1-i];
+            }
+        }
+        return result;
+    }
+
+
 }
