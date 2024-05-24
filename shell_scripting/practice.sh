a24f596171d6043c837d73c933117ea634c9df21
Manish Chauhan <manish.chauhan@ushatech.in>
2024-05-24 08:12:28 +0530

    shell : Print each argument on a new line closes #68

--- shell_scripting/practice.sh
+++ shell_scripting/practice.sh
@@ -6,9 +6,15 @@ echo "The current file name is $0"
 
 echo "The parameter 1 is $1"
 
-
 # Argument mixes string and array. Use * or separate argument.
 #echo "All arguments are $@"
 
-#preferred way
+#preferred way $* treats all arguments as a single string
 echo "All arguments are $*"
+
+echo " Printing  each argument on a new line "
+# $@ treats all args as separate
+for arg in "$@"
+  do
+    echo $arg
+  done
