8d2e33bb2052dc85022bf6e42c9c88013177daa1
Manish Chauhan <manish.chauhan@ushatech.in>
2024-05-24 08:08:42 +0530

    shell : printing file name and arguments closes #67

--- shell_scripting/practice.sh
+++ shell_scripting/practice.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "manish"
+echo "printing file name and arguments "
+
+echo "The current file name is $0"
+
+echo "The parameter 1 is $1"
+
+
+# Argument mixes string and array. Use * or separate argument.
+#echo "All arguments are $@"
+
+#preferred way
+echo "All arguments are $*"
