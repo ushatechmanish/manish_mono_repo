a27182f954e5670d4c9bb0d4c8dd1a4e795be341
Manish Chauhan <manish.chauhan@ushatech.in>
2024-05-28 20:54:24 +0530

    The Error Handling is not working in spring boot application closes #87 . Update all log messages to use {} instead of %s closes #86 Add Error Handling using ObjectMapper to extract the message from the exceptions closes  #85 Add a test in product service using WebTestClient closes #88 The Test created is failing in product service closes #89

--- spring_boot/product-service/src/test/java/in/ushatech/productservice/ProductServiceApplicationTests.java
+++ spring_boot/product-service/src/test/java/in/ushatech/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,34 @@
 package in.ushatech.productservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	@Autowired
+	private WebTestClient webClient;
+//	@Test
+//	void contextLoads() {
+//	}
+
 	@Test
-	void contextLoads() {
+	void getProductById() {
+
+		int productId = 1;
+
+		webClient.get()
+				.uri("/product/" + productId)
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.productId").isEqualTo(productId);
 	}
 
 }
